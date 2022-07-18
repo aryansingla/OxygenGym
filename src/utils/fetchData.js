d8f07af404e2086cd66e7e8d7ff5914b3a523f78
ARYAN SINGLA <72137517+aryansingla@users.noreply.github.com>
2022-07-18 22:59:02 +0530

    Update fetchData.js

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,7 +1,7 @@
 export const exerciseOptions = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': '56c698f9c9msh774bfd64b3caeccp11fceejsnd5c717f0e543',
+      'X-RapidAPI-Key': 'a4df0c7f7bmshf83c26d92bcf5dcp17aa43jsn1cf60fa9575b',
       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
     }
   };
@@ -9,7 +9,7 @@ export const exerciseOptions = {
  export const youtubeOptions = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': '56c698f9c9msh774bfd64b3caeccp11fceejsnd5c717f0e543',
+      'X-RapidAPI-Key': 'a4df0c7f7bmshf83c26d92bcf5dcp17aa43jsn1cf60fa9575b',
       'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
     }
   };
